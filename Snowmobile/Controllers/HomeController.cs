3fa78bfd6239ce2aa5d593b2dd4ab6580b382dd4
DominikJanik <dominik.janik.it@gmail.com>
2023-01-03 18:37:22 +0100

    Init

---
+++ Snowmobile/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SnowmobileShop.Data;
+
+namespace SnowmobileShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            var snowmobiles = _dbContext.Snowmobiles.ToList();
+
+            return View(snowmobiles);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var snowmobile = _dbContext.Snowmobiles.Include("SnowmobileType").FirstOrDefault(x => x.Id == id);
+            return View(snowmobile);
+        }
+    }
+}
